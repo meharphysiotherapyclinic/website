3ac1a34932b8cb6719d7baddd0d52b1110dba504
meharphysiotherapyclinic <physiogsarneja@gmail.com>
2025-08-25 12:59:54 +0530

    Create generate-sitemap.js

---
+++ generate-sitemap.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { SitemapStream, streamToPromise } = require('sitemap');
+
+const hostname = 'https://meharphysiotherapyclinic.github.io';
+const basePath = 'website';
+const files = [];
+
+function scanDir(dir) {
+  fs.readdirSync(dir).forEach(file => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      scanDir(filePath);
+    } else if (file.endsWith('.html')) {
+      let relative = path.relative(basePath, filePath).replace(/\\/g, '/');
+      files.push('/' + basePath + '/' + relative);
+    }
+  });
+}
+
+scanDir(basePath);
+
+const smStream = new SitemapStream({ hostname });
+
+files.forEach(file => {
+  const filePath = path.join(basePath, file.replace('/' + basePath + '/', ''));
+  const stats = fs.statSync(filePath);
+  const lastmod = stats.mtime.toISOString();
+  smStream.write({ url: file, changefreq: 'weekly', priority: 0.8, lastmod });
+});
+smStream.end();
+
+streamToPromise(smStream).then(data => {
+  fs.writeFileSync('website/sitemap.xml', data.toString());
+  console.log('✅ Sitemap generated successfully');
+});
